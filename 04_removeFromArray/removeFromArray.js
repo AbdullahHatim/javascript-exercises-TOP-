c7b4050e16ff2859b069b3bfa0aa201a3813dceb
Abdullah Hatim <182906341+AbdullahHatim@users.noreply.github.com>
2024-10-02 12:33:24 -0700

    feat: solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...removables) {
+  return array.filter((a) => !removables.includes(a));
 };
 
 // Do not edit below this line
