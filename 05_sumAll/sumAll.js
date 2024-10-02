4ef3817bf87afb060144dcc16d2843b85f4b6227
Abdullah Hatim <182906341+AbdullahHatim@users.noreply.github.com>
2024-10-02 13:09:03 -0700

    feat: solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (first, second) {
+  let sum = 0;
+  let biggerNumber = first > second ? first : second;
+  let smallerNumber = first < second ? first : second;
 
+  if (
+    Math.floor(smallerNumber) !== smallerNumber ||
+    Math.floor(biggerNumber) !== biggerNumber ||
+    smallerNumber < 0
+  )
+    return "ERROR";
+
+  while (biggerNumber >= smallerNumber) {
+    sum += biggerNumber;
+    biggerNumber--;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
