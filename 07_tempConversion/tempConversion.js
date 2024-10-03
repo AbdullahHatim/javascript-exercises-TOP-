d874ffa33b49028c2cae4aa5c1d1422e5c3f2cbd
Abdullah Hatim <182906341+AbdullahHatim@users.noreply.github.com>
2024-10-02 21:50:32 -0700

    feat: solve 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (F) {
+  let C = ((F - 32) * 5) / 9;
+  if (!Number.isInteger(C)) C = +C.toFixed(1);
+
+  return C;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (C) {
+  let F = (C * 9) / 5 + 32;
+  if (!Number.isInteger(F)) F = +F.toFixed(1);
+  return F;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
